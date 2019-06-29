904b8723e181270766080ef7a8b5e8d2de52e306
prestokp <prestokp@rose-hulman.edu>
2019-06-29 12:57:49 -0500

    PigGame Work in Progress

--- src/piggame/Pig.java
+++ src/piggame/Pig.java
@@ -3,26 +3,90 @@ package piggame;
 import java.util.Scanner;
 
 public class Pig {
-    public static void main(String[] args) {
-        while (!Pig.){
 
-        }
-    }
+    public static Scanner scan = new Scanner(System.in);
+
     Die dice1 = new Die();
     Die dice2 = new Die();
-    int PlayerTurn;
 
-    String Player1 = "Human";
-    String Player2 = "Computer";
-
+    public static int Player1Points = 0;
+    public static int Player2Points = 0;
+    public static int PointCounter;
     int WinThreshold = 100;
 
-    int Player1Points;
-    int Player2Points;
+    public String Player1 = "Human";
+    public String Player2 = "Computer";
+
+    public static void main(String[] args) {
+
+        System.out.println("This is the Dice Game");
+        System.out.println("Player 1 is up first...");
+        p1GameLogic();
+
+    }
+
+    int PlayerTurn;
+
+
+
+    public void p1GameLogic(){
+        PointCounter = 0;
+        System.out.println("Do you want to roll: (Y/N)");
+        String answer = scan.nextLine();
+        answer = answer.toUpperCase();
+
+        if (answer.equals("N")){
+            System.out.println("Player 2's turn");
+            p2GameLogic();
+        }
+
+        int Rolls = Roll();
+        dice1.roll();
+        dice2.roll();
+        System.out.println("Rolled Numbers: " + Rolls);
+        while (Player1Points + PointCounter < 100){
+            while (dice1.getFaceValue() != 1 || dice2.getFaceValue() != 2){
+                PointCounter = PointCounter + Rolls;
+                System.out.println("Do you want to roll: (Y/N)");
+                answer = scan.nextLine();
+                answer = answer.toUpperCase();
+            }
+            if (answer.equals("N")){
+                Player1Points = Player1Points + PointCounter;
+                System.out.println("Player 1 ended their turn");
+                //System.out.println("Scores for the game");
+                System.out.println("Player 2 roll");
+                p2GameLogic();
+            }
+
+        }
+        if (dice1.getFaceValue() == 1 || dice2.getFaceValue() == 1){
+            PointCounter = 0;
+        }
+        System.out.println("Player 1 rolled a One.");
+        System.out.println("Scores for the game");
+        System.out.println("Player 2 roll");
+        p2GameLogic();
+
+
+    }
+
+    public void p2GameLogic(){
+        PointCounter = 0;
+        System.out.println("Do you want to roll: (Y/N)");
+        String answer = scan.nextLine();
+        answer = answer.toUpperCase();
+        if (answer.equals("N")){
+            System.out.println("Player 2 skipped their turn");
+            System.out.println("Player 1 turn to roll");
+            p1GameLogic();
+        }
+        int Rolls = Roll();
+    }
 
 //Refer to player mechanics methodology on stack exchange codereview
 
-    public int Rolls(){
+    public int Roll(){
         int RollPoints;
         dice1.roll();
         dice2.roll();
@@ -36,25 +100,6 @@ public class Pig {
         String RollOrNot = keystroke.nextLine();
 
 
-    }
-
-    public boolean Gameover(int WinThreshold, int Player1Points, int Player2Points){
-        boolean Player1Victory = true;
-        boolean Player2Victory = true;
-
-        if (Player1Points >= WinThreshold){
-            return Player1Victory;
-        }
-
-        if (Player2Points >= WinThreshold){
-            return Player2Victory;
-        }
-
-        Pig(){
-            Player1Points = 0;
-            Player2Points = 0;
-            PlayerTurn = 1;
-        }
     }
 
 
