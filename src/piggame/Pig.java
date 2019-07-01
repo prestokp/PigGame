e21c2292c059762bf1f35b715f3b7618002ac862
prestokp <prestokp@rose-hulman.edu>
2019-06-30 19:40:45 -0500

    PigGame Work in Progress

--- src/piggame/Pig.java
+++ src/piggame/Pig.java
@@ -15,29 +15,45 @@ public class Pig {
         int roundScore; //This variable is to keep the score in individual rounds
 
         public String askRoll(){
+            System.out.println("Do you want to roll(Y/N): ");  //This code prompts the user if they want to run again
+            Scanner scan = new Scanner(System.in);            //This reads in the input Y or N for Yes or No
+            String answer = scan.nextLine();
+            answer = answer.toUpperCase();
+
+            if (answer.equals("Y")){
+                ControlFlow = true;
+                return ControlFlow;
+            }
+
+            else{
+                ControlFlow = false;
+                return;
+            }
 
         }
-        while (humanScore < VictoryNumber || computerScore < VictoryNumber){
+
+        while (humanScore < VictoryNumber || computerScore < VictoryNumber) {
 
             boolean ControlFlow = true;
 
-            if (ControlFlow = true){
+            if (ControlFlow = true) {
 
-                while(humanRoll){
+                while (ControlFlow = true) {
                     humanScore = 0;
                     dice1.roll();
                     dice2.roll();
 
                     //The condition in which both values of the die can be added
-                    if (dice1.getFaceValue() != 1 && dice2.getFaceValue() != 1){
+                    if (dice1.getFaceValue() != 1 && dice2.getFaceValue() != 1) {
                         int sum;
                         sum = dice1.getFaceValue() + dice2.getFaceValue();
                         humanScore = humanScore + sum;
+                        askRoll();
                     }
 
                     //The condition in which one value of the die is 1, no points
                     //added for this round
-                    if (dice1.getFaceValue() == 1 || dice2.getFaceValue() == 1){
+                    if (dice1.getFaceValue() == 1 || dice2.getFaceValue() == 1) {
                         int noPoints = 0;
                         humanScore = humanScore + noPoints;
                         ControlFlow = false;
@@ -45,27 +61,80 @@ public class Pig {
 
                     //The condition in which both values of the die are 1
                     //Eliminates all accumulated points by assigning the points to 0
-                    if (dice1.getFaceValue() == 1 && dice2.getFaceValue() == 1){
+                    if (dice1.getFaceValue() == 1 && dice2.getFaceValue() == 1) {
                         int snakeEyes = 0;
                         humanScore = snakeEyes;
                         ControlFlow = false; //probably going to replace with a function that can control the methods
                     }
 
-                    if (ControlFlow = false){
+                    //Code below this point mostly pertains to the functionality of the computer
+                    if (ControlFlow = false) {
                         computerScore = 0;
-                        int computerRoundScore; //This keeps track the score for the computer per round
-                                                //If the
-                    }
-
-                } //While loop delimiter
-
-            }
-        }
 
 
+                        while (ControlFlow = false){
+                            int computerRoundScore = 0; //This keeps track the score for the computer per round
+                                                        //If the score gets above 20 the computer no longer rolls
+                            dice1.roll();
+                            dice2.roll();
+
+                            if (dice1.getFaceValue() != 1 && dice1.getFaceValue() != 1){
+                                int sum;
+                                sum = dice1.getFaceValue() + dice2.getFaceValue();
+                                computerScore += sum;
+                                computerRoundScore += sum;
+                                if (computerRoundScore >= 20){
+                                    ControlFlow = true;
+                                }
+                            }
+
+                            if (dice1.getFaceValue() == 1 || dice2.getFaceValue() == 1){
+                                int noPoints = 0; //I don't need a variable, but if the rules of the game changed, variables make
+                                                  //modularity convenient
+                                computerScore += noPoints;
+                                ControlFlow = true;
+                            }
+
+                            if (dice1.getFaceValue() == 1 && dice2.getFaceValue() == 1) {
+                                int snakeEyes = 0; //I don't need a variable, but if the rules of the game changed, variables make
+                                                   //modularity convenient
+                                computerScore = snakeEyes;
+                                ControlFlow = true;
+                            }
+
+                        } //Computer while loop delimiter
+
+                    } //Computer Control flow delimiter
+
+                } //While loop delimiter for the rolling humans
+
+            } //Control flow conditional delimiter
+
+        } //While loop delimiter for the entire game
+
+    } //Delimiter for the main function
+
+} //Delimiter for the Pig Public Class
 
 
-        }//Delimiter for the main function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /*
     public int rollDice(){
         dice1.roll();
